18faa85567eae5af8779d1137cb65aade309af46
oswaldosalazar <oswaldo.r.salazar@gmail.com>
2017-02-11 19:55:55 -0700

    Added toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  product1 = 'Product 1';
+  product2 = 'Product 2';
+  product3 = 'Product 3';
 }
